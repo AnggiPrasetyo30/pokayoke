082e8e7ec04173a84086bfbc7b675de8aa918bb4
AnggiPrasetyo30 <65654499+AnggiPrasetyo30@users.noreply.github.com>
2022-02-04 08:05:07 +0700

    layout create user

--- app/src/main/java/com/example/hyundai/activity/list_user.java
+++ app/src/main/java/com/example/hyundai/activity/list_user.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.hyundai.R;
 import com.example.hyundai.SQLite.DatabaseHelper;
@@ -33,5 +36,14 @@ public class list_user extends AppCompatActivity {
         adDataf = new recycler_user(this, itemP);
         adDataf.notifyDataSetChanged();
         recyclerView.setAdapter(adDataf);
+
+        Button btnCreate = findViewById(R.id.btnBuat);
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(list_user.this, create_user.class);
+                startActivity(intent);
+            }
+        });
     }
 }
